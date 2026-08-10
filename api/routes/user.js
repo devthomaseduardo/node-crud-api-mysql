3b2a5a0949d34f0c67cf3d8ae3bdedbf73653828
Thomas Eduardo <developer.thomas@outlook.com.br>
2026-08-10 13:58:08 -0300

    add complete user routes

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,14 +1,18 @@
 import express from "express";
-import { addUser, deleteUser, getUsers, updateUser } from "../controllers/user.js";
+import {
+  addUser,
+  deleteUser,
+  getUserById,
+  getUsers,
+  updateUser,
+} from "../controllers/user.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/", getUsers)
+router.get("/", getUsers);
+router.get("/:id", getUserById);
+router.post("/", addUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
-router.post("/", addUser)
-
-router.put("/:id", updateUser)
-
-router.delete("/:id", deleteUser)
-
-export default router
+export default router;
